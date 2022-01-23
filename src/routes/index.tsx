d3564c1be7c9774e2d775849ee9f38c743ed08a2
Felicia <akufelicia@gmail.com>
2022-01-24 05:11:11 +0700

    Static page for order

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "../pages/homepage";
 import Product from "../pages/product";
 import DashboardProduct from "../pages/Dashboard/Product";
+import Checkout from "../pages/Checkout";
 const Router: React.FC = (props: any) => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="product/:slug" element={<Product />} />
-        <Route path="dashboard/product" element={<DashboardProduct />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="product/:slug" element={<Product />} />
+				<Route
+					path="dashboard/product"
+					element={<DashboardProduct />}
+				/>
+				<Route path="checkout" element={<Checkout />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
