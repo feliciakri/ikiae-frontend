83359f52226b40905b5f12d6deebe60c9da44fbe
Felicia <akufelicia@gmail.com>
2022-01-25 13:23:42 +0700

    Filter by categories

--- src/components/Card/CardProduct.tsx
+++ src/components/Card/CardProduct.tsx
@@ -7,60 +7,63 @@ import axios from "axios";
 import { formatRupiah } from "../../hooks/useFormatIDR";
 
 const CardProduct = ({ product }: any) => {
-  const { state } = useContext(AuthContext);
-  const { isLogged, token } = state;
+	const { state } = useContext(AuthContext);
+	const { isLogged, token } = state;
 
-  const price = formatRupiah(product.price);
-  const products = {
-    product_id: product.id,
-    quantity: 1,
-  };
-  const addProductHandler = () => {
-    axios
-      .post(`${process.env.REACT_APP_API_KEY}/carts`, products, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => console.log(res));
-  };
+	const price = formatRupiah(product.price);
+	const products = {
+		product_id: product.id,
+		quantity: 1,
+	};
+	const addProductHandler = () => {
+		axios
+			.post(`${process.env.REACT_APP_API_KEY}/carts`, products, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => console.log(res));
+	};
 
-  return (
-    <div className="flex flex-col justify-between shadow hover:shadow-md w-full bg-white rounded-lg overflow-hidden cursor-pointer">
-      <img
-        className="object-fill w-full h-48"
-        src={product.image || "/logo192.png"}
-        alt={product.name}
-      />
-      <div className="relative px-4 font-inter">
-        <Link to={`/product/${product.id}`}>
-          <h3 className="font-semibold text-base md:text-xl text-gray-800">
-            {product.name}
-          </h3>
-          <p className="mt-4 text-base md:text-lg text-gray-600">
-            {product.description}
-          </p>
-        </Link>
-      </div>
+	return (
+		<div className="flex flex-col justify-between shadow hover:shadow-md w-full bg-white rounded-lg overflow-hidden cursor-pointer">
+			<img
+				className="object-contain w-full"
+				src={product.image || "/logo192.png"}
+				alt={product.name}
+			/>
+			<div className="relative px-4 font-inter">
+				<Link to={`/product/${product.id}`}>
+					<h3 className="font-semibold text-base md:text-xl text-gray-800">
+						{product.name}
+					</h3>
+					<p className="mt-4 text-base md:text-lg text-gray-600">
+						{product.description}
+					</p>
+				</Link>
+			</div>
 
-      <div className="p-4">
-        <h3 className="py-3 font-bold text-lg">{price}</h3>
-        {isLogged ? (
-          <div className="bg-gray-200 w-full py-2 " onClick={addProductHandler}>
-            <div className="mx-3 flex flex-row items-center">
-              <span className="w-1/5">
-                <DownloadIcon className="w-5" />
-              </span>
-              Add to Cart
-            </div>
-          </div>
-        ) : (
-          <ButtonCart />
-        )}
-      </div>
-    </div>
-  );
+			<div className="p-4">
+				<h3 className="py-3 font-bold text-lg">{price}</h3>
+				{isLogged ? (
+					<div
+						className="bg-gray-200 w-full py-2 "
+						onClick={addProductHandler}
+					>
+						<div className="mx-3 flex flex-row items-center">
+							<span className="w-1/5">
+								<DownloadIcon className="w-5" />
+							</span>
+							Add to Cart
+						</div>
+					</div>
+				) : (
+					<ButtonCart />
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default CardProduct;
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -5,55 +5,82 @@ import CardProduct from "../../components/Card/CardProduct";
 import { AuthContext } from "../../context/AuthContext";
 
 const Homepage: React.FC = () => {
-  const [isProducts, setIsProducts] = useState<Array<any>>();
-  const [isCategories, setIsCategories] = useState<Array<any>>();
+	const [isProducts, setIsProducts] = useState<Array<any>>();
+	const [isCategories, setIsCategories] = useState<Array<any>>();
 
-  // Filtered Category
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_KEY}/products`)
-      .then((response) => setIsProducts(response.data));
-    axios
-      .get(`${process.env.REACT_APP_API_KEY}/categories`)
-      .then((response) => setIsCategories(response.data));
-  }, []);
+	// Filtered Category
+	useEffect(() => {
+		axios
+			.get(`${process.env.REACT_APP_API_KEY}/products`)
+			.then((response) => setIsProducts(response.data));
+		axios
+			.get(`${process.env.REACT_APP_API_KEY}/categories`)
+			.then((response) => setIsCategories(response.data));
+	}, []);
 
-  const [filteredSearchProducts, setFilteredSearchProducts] = useState<any>([]);
-  const [search, setSearch] = useState("");
-  useEffect(() => {
-    setFilteredSearchProducts(
-      isProducts?.filter((product) =>
-        product.name.toLowerCase().includes(search.toLowerCase())
-      )
-    );
-  }, [search, isProducts]);
-  return (
-    <Layout onSearch={setSearch}>
-      <div className="flex flex-col sm:flex-row">
-        <div className="md:w-1/3 ">
-          <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 px-5 gap-5 mb-16">
-            {isCategories?.map((product, i) => (
-              <button
-                className="bg-white py-2 px-1 flex flex-col items-center justify-between"
-                value={product.category_id}
-                key={i}
-              >
-                <img src={product.image} alt={product.name} />
-                <p>{product.name}</p>
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="md:w-2/3">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {filteredSearchProducts?.map((product: any, i: number) => {
-              return <CardProduct key={i} product={product} />;
-            })}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+	const [filteredSearchProducts, setFilteredSearchProducts] = useState<any>(
+		[]
+	);
+	const [search, setSearch] = useState("");
+	useEffect(() => {
+		setFilteredSearchProducts(
+			isProducts?.filter((product) =>
+				product.name.toLowerCase().includes(search.toLowerCase())
+			)
+		);
+	}, [search, isProducts]);
+
+	//const [categoryFilter, setCategoryFilter] = useState("");
+
+	// useEffect(() => {
+	// 	setFilteredCategoryProducts(
+	// 		isProducts?.filter((product) =>
+	// 			product.category_id.includes(categoryFilter.toLowerCase())
+	// 		)
+	// 	);
+	// }, [categoryFilter, isProducts]);
+
+	const filterCategory = (curcat: any) => {
+		const id = curcat.id;
+		console.log(isProducts);
+		const newProducts = isProducts?.filter((newVal) => {
+			return newVal.category_id === id;
+		});
+		setFilteredSearchProducts(newProducts);
+	};
+
+	return (
+		<Layout onSearch={setSearch}>
+			<div className="flex flex-col sm:flex-row">
+				<div className="md:w-1/3 ">
+					<div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 px-5 gap-5 mb-16">
+						{isCategories?.map((category, i) => (
+							<button
+								className="bg-white py-2 px-1 flex flex-col items-center justify-between"
+								value={category.category_id}
+								key={i}
+								onClick={() => filterCategory(category)}
+							>
+								<img src={category.image} alt={category.name} />
+								<p>{category.name}</p>
+							</button>
+						))}
+					</div>
+				</div>
+				<div className="md:w-2/3">
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+						{filteredSearchProducts?.map(
+							(product: any, i: number) => {
+								return (
+									<CardProduct key={i} product={product} />
+								);
+							}
+						)}
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Homepage;
